c51087f0ba38b38f2ea65f9a7016d576acafe5ac
joaopedrowanderley <43885710+joaopedrowanderley@users.noreply.github.com>
2020-10-23 14:38:11 -0300

    inserindo classe venda

---
+++ src/br/com/sistemalanchonete/model/Venda.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistemalanchonete.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author joão pedro
+ */
+public class Venda {
+    private int codigo;
+    private Date data;
+    private float valor;
+    private int cod_produto;
+    private int cod_cliente;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getCod_produto() {
+        return cod_produto;
+    }
+
+    public void setCod_produto(int cod_produto) {
+        this.cod_produto = cod_produto;
+    }
+
+    public int getCod_cliente() {
+        return cod_cliente;
+    }
+
+    public void setCod_cliente(int cod_cliente) {
+        this.cod_cliente = cod_cliente;
+    }
+    
+    
+}
